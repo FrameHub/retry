88f58dc572ea8dea1161457282afe078c1dbbc13
cssmagic <cssmagic.cn@gmail.com>
2022-06-09 00:48:17 +0800

    test: 为 retry() 补充测试代码

--- src/retry.js
+++ src/retry.js
@@ -5,6 +5,14 @@ function _sleep(time) {
 	})
 }
 
+const MSG = {
+	ARG_VALIDATOR__FN: '[Retry] argument `validator` is not a function',
+	ARG_RETRIES__INT: '[Retry] argument `retries` must be an integer',
+	ARG_RETRIES__POS: '[Retry] argument `retries` must be greater than 0',
+	ARG_TIMEOUT__NUM: '[Retry] argument `timeout` must be a number',
+	ARG_TIMEOUT__POS: '[Retry] argument `timeout` must be greater than 0',
+}
+
 /**
  * 一个简单的重试方法
  * @param {function} validator 函数或异步函数，检查器，判断是否满足检查条件
@@ -12,25 +20,25 @@ function _sleep(time) {
  * - options.retries {Number} 最大重试次数，默认5次
  * - options.timeout {Number} 重试间隔(毫秒)，默认10秒
  */
-module.exports = async function retry(validator, {
+async function retry(validator, {
 	retries = 5,
 	timeout = 10000,
 } = {}) {
 
 	if (typeof validator !== 'function') {
-		throw Error('[Retry] argument `validator` is not a function')
+		throw Error(MSG.ARG_VALIDATOR__FN)
 	}
 	if (!Number.isInteger(retries)) {
-		throw Error('[Retry] argument `retries` must be an integer')
+		throw Error(MSG.ARG_RETRIES__INT)
 	}
 	if (retries < 0) {
-		throw Error('[Retry] argument `retries` must be greater than 0')
+		throw Error(MSG.ARG_RETRIES__POS)
 	}
 	if (typeof timeout !== 'number') {
-		throw Error('[Retry] argument `timeout` must be a number')
+		throw Error(MSG.ARG_TIMEOUT__NUM)
 	}
 	if (timeout < 0) {
-		throw Error('[Retry] argument `timeout` must be greater than 0')
+		throw Error(MSG.ARG_TIMEOUT__POS)
 	}
 
 	let times = 0
@@ -47,3 +55,8 @@ module.exports = async function retry(validator, {
 		throw new Error(`[Retry] tried ${times} time(s) and failed`)
 	}
 }
+
+module.exports = retry
+
+// for testing
+retry.__MSG = MSG
